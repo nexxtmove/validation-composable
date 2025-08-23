9bd44b05af5bbc21b572b5a319b8141251d33bff
N1cE-me <banicov2011@yandex.ru>
2025-08-23 17:40:12 +0500

    feat: Change issues type for union schemas
    - Add Combine type
    - Update Issues type

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,15 @@ import { reactive, watch, unref, type MaybeRef } from 'vue'
 
 type Data = Record<string, unknown>
 
+type Combine<
+  T,
+  K extends PropertyKey = T extends unknown ? keyof T : never,
+> = T extends unknown ? T & Partial<Record<Exclude<K, keyof T>, never>> : never
+
 type Issues<T> = {
-  [Key in keyof T]?: T[Key] extends object ? Issues<T[Key]> : string[]
+  [Key in keyof Combine<T>]?: Combine<T>[Key] extends object
+    ? Issues<Combine<T>[Key]>
+    : string[]
 }
 
 export function useValidation<T extends Data>(
